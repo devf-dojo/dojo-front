3d3f65e4e830ce63e2ad4032e2375c6b467f5ce0
Fabian Magallanes <fabi.maga57@gmail.com>
2017-07-24 21:39:06 -0500

    fixed the ci script

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,9 +7,10 @@ import renderer from 'react-test-renderer'
 import App from '../pages/index.js'
 
 describe('With Enzyme', () => {
-  it('App shows "Hello world!"', () => {
+  it('Check headers text', () => {
     const app = shallow(<App />)
 
-    expect(app.find('p').text()).toEqual('Hello World!')
+    expect(app.find('h1').text()).toEqual('Dojo Dev.f')
+    expect(app.find('h3').text()).toEqual('El siguiente paso en tu carrera de desarrollador')
   })
 })
