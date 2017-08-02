097e17d220827843c35abf12f8d9974026c9f9d5
Bernardo Cassina Buerba <bernardocassinabuerba@Bernardos-MacBook-Pro.local>
2017-08-02 09:09:07 -0500

    Ajax functions to communicate with the API and with slack.

--- pages/login.vue
+++ pages/login.vue
@@ -1,18 +1,61 @@
-<template id="">
+<template id="" xmlns:v-on="http://www.w3.org/1999/xhtml">
   <div class="">
-
-    <a class="waves-effect waves-light btn" v-on:click="login()"><i class="material-icons right"></i>log in
-  </a>
-
-
-
-
+    <a class="waves-effect waves-light btn" v-on:click="login()"><i class="material-icons right"></i>log in</a>
   </div>
-
 </template>
-<script>
 
+<script>
 import * as firebase from 'firebase'
+
+var sendSlack = function (msg) {
+    var webhookURI = 'https://hooks.slack.com/services/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';
+
+    $.ajax(
+        {
+          type: 'POST',
+          dataType: 'json',
+          url: webhookURI,
+          data: 'payload=' + JSON.stringify({
+                  text: msg
+            })
+        }
+    ).done(function () {
+       console.log('DONE')
+    }).fail(function (error) {
+        console.log(error)
+    }).always(function () {
+        console.log('COMPLETE')
+    })
+};
+
+
+
+
+var getUserAPI = function (uid) {
+    var url = 'https://us-central1-devf-dojo-admin.cloudfunctions.net/api/v1/dojo/get_user';
+    var payload = JSON.stringify(uid);
+    console.log(payload)
+
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: payload,
+        contentType:"application/json; charset=utf-8",
+        statusCode: {
+            404: function (error) {
+                sendSlack('No se encontro el recurso en la API ' +  url + ' ' + error)
+            }
+        }
+    }).done(function (result) {
+        console.log('SUCCESS')
+        console.log(result)
+
+
+    }).fail(function (error) {
+        sendSlack('Ocurrio un error en la peticion a ' + url + ' '  + error)
+    });
+};
+
 
 export default {
 
@@ -25,7 +68,14 @@ export default {
     login () {
       var provider = new firebase.auth.GithubAuthProvider()
       firebase.auth().signInWithPopup(provider).then(function (result) {
-          console.log(result)
+          var uid = result.user.uid;
+          console.log(uid + 'UUID')
+          getUserAPI({uid});
+
+      }, function (error) {
+
+          console.log(error.code)
+          console.log(error)
       })
     }
   },
@@ -37,7 +87,8 @@ export default {
         projectId: "devf-dojo-admin",
         storageBucket: "devf-dojo-admin.appspot.com",
         messagingSenderId: "183887932653"
-      }
+      };
+
       if(!firebase.apps.length){
         firebase.initializeApp(config)
       }
