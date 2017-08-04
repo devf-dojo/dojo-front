7831ac9ed4fcda9e0cfde782e86227f87e8f9e3c
Fabian Magallanes <fabi.maga57@gmail.com>
2017-08-04 15:42:59 -0500

    Modifique colores

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <nav>
-      <div class="nav-wrapper">
-        <a href="#" class="brand-logo">Logo</a>
+      <div class="nav-wrapper" id="up">
+        <a href="#" class="brand-logo">Dojo</a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
           <li><a href="/login">login</a></li>
           <li><a href="badges.html">Components</a></li>
