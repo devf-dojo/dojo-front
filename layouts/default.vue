64c005f356a9cc2f44b7612e323841b7d3e85ff7
Bernardo Cassina Buerba <bernardocassinabuerba@Bernardos-MacBook-Pro.local>
2017-08-07 21:10:38 -0500

    Some fixes.

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,6 @@
         <a href="#" class="brand-logo">Dojo</a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
           <li><a href="/login">login</a></li>
-          <li><a href="/hello">login</a></li>
           <li><a href="badges.html">Components</a></li>
           <li><a href="collapsible.html">JavaScript</a></li>
         </ul>
