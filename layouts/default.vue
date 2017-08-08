b8a007e160d2d2f2bfacea10a3ae548ce2c9ee02
Bernardo Cassina Buerba <bernardocassinabuerba@Bernardos-MacBook-Pro.local>
2017-08-07 21:41:07 -0500

    Removed unused route.

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,7 @@
       <div class="nav-wrapper" id="up">
         <a href="#" class="brand-logo">Dojo</a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><a href="/login">login</a></li>
+          <li><a href="/login">login</a></li> 
           <li><a href="badges.html">Components</a></li>
           <li><a href="collapsible.html">JavaScript</a></li>
         </ul>
