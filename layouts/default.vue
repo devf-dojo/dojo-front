5aa221cf4d3a8d7b2a57dc3f723955a0f3450bd9
Bernardo Cassina Buerba <bernardocassinabuerba@Bernardos-MBP.Home>
2017-08-08 21:38:41 -0500

    Reverted deleted import.

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,5 +11,7 @@
       </div>
     </nav>
     <nuxt/>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/js/materialize.min.js"></script>
   </div>
 </template>
