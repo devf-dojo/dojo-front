5fc97dfe2141713b77b033fa97b91f80cd709e61
manolobrn <e.beltranfs@gmail.com>
2017-08-16 21:56:47 -0500

    v1 Public View

--- pages/public.vue
+++ pages/public.vue
@@ -4,15 +4,39 @@
 	    <!-- Page Content goes here -->
 		<div class="card medium">
 		    <div class="card-image waves-effect waves-block waves-light">
-		      <img class="activator" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Bitcoin.png/240px-Bitcoin.png">
+		      <img class="activator" src="profile.jpg">
 		    </div>
 		    <div class="card-content">
 		      <span class="card-title activator grey-text text-darken-4">Manuel Torres - Atlanta<i class="material-icons right">more_vert</i></span>
-		      <p>Hola, soy un programador web</p>
+		      <p class="left-align">
+		      	<b>Telefono:</b> 556793169 
+		      	<br>
+		      	<b>Email:</b> e.beltranfs@gmail.com
+		      </p>
+		     
 		    </div>
+		    <div class="card-action">
+              <a href="#">WebSite</a>
+              <a href="#">Facebook</a>
+              <a href="#">Twitter</a>
+              <a href="#">Linkedin</a>
+              <a href="#">Github</a>
+            </div>
 		    <div class="card-reveal">
 		      <span class="card-title grey-text text-darken-4">Manuel Torres - Atlanta<i class="material-icons right">close</i></span>
-		      <p>Telefono: 55600793169</p>
+		        <div class="row">
+					<div class="col s6 m6 l6">
+						<p class="left-align"><b>Skills</b></p>
+						<div class="chip">Leadership</div>
+						<div class="chip">Python</div>
+						<div class="chip">Django</div>
+						<div class="chip">Blockchain</div>
+					</div>
+					<div class="col s6 m6 l6">
+						
+					</div>
+				</div>
+ 			 </div>
 		    </div>
 	  </div>
 	</div>
